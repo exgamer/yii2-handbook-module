8efdcafa6c6b738b0b0d34d5b36e9da8a68b6d5e
citizenzet <exgamer@live.ru>
2019-11-07 19:32:03 +0600

    cd

---
+++ src/web/controllers/EntityTypeController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace concepture\yii2handbook\web\controllers;
+
+use concepture\yii2user\enum\UserRoleEnum;
+use concepture\yii2logic\actions\web\AutocompleteListAction;
+
+/**
+ * Class EntityTypeController
+ * @package concepture\yii2handbook\web\controllers
+ * @author Olzhas Kulzhambekov <exgamer@live.ru>
+ */
+class EntityTypeController extends Controller
+{
+
+}
