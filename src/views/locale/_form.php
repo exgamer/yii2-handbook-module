9d31854721ad5466d131b291672c2a84ff26e9fd
citizenzet <exgamer@live.ru>
2019-11-08 15:58:31 +0600

    cd

---
+++ src/views/locale/_form.php
@@ -0,0 +1,21 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+
+?>
+
+<div class="user-form">
+
+    <?php $form = ActiveForm::begin(); ?>
+
+    <?= $form->field($model, 'locale')->textInput(['maxlength' => true]) ?>
+    <?= $form->field($model, 'sort')->textInput(['maxlength' => true]) ?>
+
+    <div class="form-group">
+        <?= Html::submitButton(Yii::t('user', 'Сохранить'), ['class' => 'btn btn-success']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+
+</div>
---
+++ src/web/controllers/LocaleController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace concepture\yii2handbook\web\controllers;
+
+
+/**
+ * Class LocaleController
+ * @package concepture\yii2handbook\web\controllers
+ * @author Olzhas Kulzhambekov <exgamer@live.ru>
+ */
+class LocaleController extends Controller
+{
+
+}
