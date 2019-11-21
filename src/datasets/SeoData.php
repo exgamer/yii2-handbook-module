ff3bc606ada8e6b827a0ee474f3567b52fdd378d
citizenzet <exgamer@live.ru>
2019-11-21 12:02:04 +0600

    cd

---
+++ src/datasets/SeoData.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace concepture\yii2handbook\datasets;
+
+use yii\base\BaseObject;
+
+/**
+ * Class SeoData
+ * @package concepture\yii2handbook\datasets
+ * @author Olzhas Kulzhambekov <exgamer@live.ru>
+ */
+class SeoData extends BaseObject
+{
+    public $seo_h1;
+    public $seo_title;
+    public $seo_description;
+    public $seo_keywords;
+    public $seo_text;
+}
--- src/services/SeoSettingsService.php
+++ src/services/SeoSettingsService.php
@@ -2,6 +2,8 @@
 namespace concepture\yii2handbook\services;
 
 use concepture\yii2logic\services\Service;
+use yii\db\ActiveQuery;
+use Yii;
 
 /**
  * Class SeoSettingsService
@@ -10,5 +12,28 @@ use concepture\yii2logic\services\Service;
  */
 class SeoSettingsService extends Service
 {
+    /**
+     * Возвращает настройки SEO для текущей страницы и дефолтные с учетом чзыка приложения
+     *
+     * @return array
+     * @throws \yii\base\InvalidConfigException
+     */
+    public function getSeoForCurrentUrl()
+    {
+        $current = Yii::$app->getRequest()->getPathInfo();
+        $md5 = md5($current);
 
+        return $this->getAllByCondition(function(ActiveQuery $query) use ($md5){
+            $query->andWhere("(url_md5_hash IN (:url_md5_hash, NULL)",
+                [
+                    ':url_md5_hash' => $md5
+                ]
+            );
+            $query->andWhere("(locale = :locale",
+                [
+                    ':locale' => Yii::$app->language
+                ]
+            );
+        });
+    }
 }
