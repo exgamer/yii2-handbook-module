0e5e0d4958e09497b7849743901d6c2fcac996d4
citizenzet <exgamer@live.ru>
2019-11-21 14:37:42 +0600

    cd

--- src/services/SeoSettingsService.php
+++ src/services/SeoSettingsService.php
@@ -1,7 +1,11 @@
 <?php
 namespace concepture\yii2handbook\services;
 
+use concepture\yii2handbook\converters\LocaleConverter;
+use concepture\yii2handbook\datasets\SeoData;
+use concepture\yii2logic\helpers\DataLoadHelper;
 use concepture\yii2logic\services\Service;
+use yii\base\InvalidConfigException;
 use yii\db\ActiveQuery;
 use Yii;
 
@@ -13,10 +17,47 @@ use Yii;
 class SeoSettingsService extends Service
 {
     /**
+     * Возвращает настройки SEO для текущей страницы
+     *
+     * @param null $model
+     * @return SeoData|mixed
+     * @throws InvalidConfigException
+     */
+    public function getSeoDataSet($model = null)
+    {
+        $defaultSeoSetting = null;
+        $pageCustomSeoSetting =null;
+        $dataSet = new SeoData();
+        $defaultSeoSettings = $this->getSeoForCurrentUrl();
+        foreach ($defaultSeoSettings as $seoSetting){
+            if (empty($seoSetting->url)){
+                $defaultSeoSetting = $seoSetting;
+                continue;
+            }
+
+            $pageCustomSeoSetting = $seoSetting;
+        }
+
+        if ($defaultSeoSetting){
+            $dataSet = DataLoadHelper::loadData($defaultSeoSetting, $dataSet, true);
+        }
+
+        if ($model){
+            $dataSet = DataLoadHelper::loadData($model, $dataSet, true);
+        }
+
+        if ($pageCustomSeoSetting){
+            $dataSet = DataLoadHelper::loadData($pageCustomSeoSetting, $dataSet, true);
+        }
+
+        return $dataSet;
+    }
+
+    /**
      * Возвращает настройки SEO для текущей страницы и дефолтные с учетом чзыка приложения
      *
      * @return array
-     * @throws \yii\base\InvalidConfigException
+     * @throws InvalidConfigException
      */
     public function getSeoForCurrentUrl()
     {
@@ -24,16 +65,17 @@ class SeoSettingsService extends Service
         $md5 = md5($current);
 
         return $this->getAllByCondition(function(ActiveQuery $query) use ($md5){
-            $query->andWhere("(url_md5_hash IN (:url_md5_hash, NULL)",
+            $query->andWhere("url_md5_hash = :url_md5_hash OR url_md5_hash IS NULL",
                 [
                     ':url_md5_hash' => $md5
                 ]
             );
-            $query->andWhere("(locale = :locale",
+            $query->andWhere("locale = :locale",
                 [
-                    ':locale' => Yii::$app->language
+                    ':locale' => LocaleConverter::key(Yii::$app->language)
                 ]
             );
+            $query->orderBy('url');
         });
     }
 }
