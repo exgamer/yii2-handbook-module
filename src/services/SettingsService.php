bbdfac8ae2116e04f1524475514349224c6c5c58
citizenzet <exgamer@live.ru>
2019-11-21 17:26:16 +0600

    cd

--- src/services/SettingsService.php
+++ src/services/SettingsService.php
@@ -1,8 +1,12 @@
 <?php
 namespace concepture\yii2handbook\services;
 
+use concepture\yii2handbook\converters\LocaleConverter;
+use concepture\yii2logic\enum\IsDeletedEnum;
 use concepture\yii2logic\enum\StatusEnum;
 use concepture\yii2logic\services\Service;
+use yii\db\ActiveQuery;
+use Yii;
 
 /**
  * Class SettingsService
@@ -11,5 +15,18 @@ use concepture\yii2logic\services\Service;
  */
 class SettingsService extends Service
 {
-
+    /**
+     * Для расширения запроса для вывода каталога и списка для выпадашек
+     *
+     * @param ActiveQuery $query
+     */
+    public function extendCatalogTraitQuery(ActiveQuery $query)
+    {
+        $domainId = Yii::$app->domainService->getCurrentDomainId();
+        $sql = "(domain_id = :domain_id OR domain_id IS NULL)";
+        $query->andWhere($sql, [':domain_id' => $domainId]);
+        $locale = LocaleConverter::key(Yii::$app->language);
+        $sql = "(locale = :locale OR locale IS NULL)";
+        $query->andWhere($sql, [':locale' => $locale]);
+    }
 }
