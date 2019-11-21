a93fa65a5ee20475900c3a93183b2b7c086fd7bd
citizenzet <exgamer@live.ru>
2019-11-21 16:46:35 +0600

    cd

--- src/views/settings/index.php
+++ src/views/settings/index.php
@@ -50,7 +50,7 @@ $this->params['breadcrumbs'][] = $this->title;
 
             [
                 'class'=>'yii\grid\ActionColumn',
-                'template'=>'{view} {update}',
+                'template'=>'{view} {update} {delete}',
             ],
         ],
     ]); ?>
