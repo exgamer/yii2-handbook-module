c2d5fe918e72794b67336679fd1f57043ab787c3
citizenzet <exgamer@live.ru>
2019-11-27 11:48:55 +0600

    cd

--- src/console/migrations/m191125_101549_tags.php
+++ src/console/migrations/m191125_101549_tags.php
@@ -31,6 +31,7 @@ class m191125_101549_tags extends Migration
         $this->addIndex(['locale']);
         $this->addIndex(['type']);
         $this->addIndex(['is_deleted']);
+        $this->addUniqueIndex(['caption']);
         $this->addForeign('user_id', 'user','id');
         $this->addForeign('domain_id', 'domain','id');
         $this->addForeign('locale', 'locale','id');
