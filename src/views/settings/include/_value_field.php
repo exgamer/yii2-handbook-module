8dcb81c50d96b1672aa6a24a874c0d59b3e3c4f6
citizenzet <exgamer@live.ru>
2019-12-03 17:46:04 +0600

    cd

--- src/views/settings/include/_value_field.php
+++ src/views/settings/include/_value_field.php
@@ -1,6 +1,30 @@
 <?php
     use concepture\yii2handbook\enum\SettingsTypeEnum;
+    use kamaelkz\yii2admin\v1\widgets\formelements\editors\froala\FroalaEditor;
 ?>
 <?php if ($model->type == SettingsTypeEnum::TEXT) : ?>
     <?= $form->field($model, 'value')->textInput(['maxlength' => true]) ?>
+<?php endif;?>
+
+<?php if ($model->type == SettingsTypeEnum::TEXT_AREA) : ?>
+    <?= $form->field($model, 'value')->textarea(); ?>
+<?php endif;?>
+
+<?php if ($model->type == SettingsTypeEnum::FROALA) : ?>
+    <?= $form
+        ->field($model, 'value')
+        ->widget(FroalaEditor::class, [
+            'model' => $model,
+            'attribute' => 'editor',
+            'clientOptions' => [
+                'attribution' => false,
+                'heightMin' => 200,
+                'toolbarSticky' => true,
+                'toolbarInline'=> false,
+                'theme' =>'royal', //optional: dark, red, gray, royal
+                'language' => Yii::$app->language,
+                'quickInsertTags' => [],
+            ]
+        ]);
+    ?>
 <?php endif;?>
