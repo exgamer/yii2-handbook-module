0cabd5ca8df5fcd67c99ffef7608b80c08625deb
citizenzet <exgamer@live.ru>
2019-12-18 11:58:39 +0600

    cd

--- src/services/SettingsService.php
+++ src/services/SettingsService.php
@@ -1,11 +1,13 @@
 <?php
 namespace concepture\yii2handbook\services;
 
+use concepture\yii2handbook\enum\SettingsTypeEnum;
 use concepture\yii2logic\enum\IsDeletedEnum;
 use concepture\yii2logic\enum\StatusEnum;
 use concepture\yii2logic\services\Service;
 use yii\db\ActiveQuery;
 use Yii;
+use Exception;
 use concepture\yii2handbook\traits\ServicesTrait as HandbookServices;
 use concepture\yii2handbook\services\traits\ReadSupportTrait;
 use concepture\yii2handbook\services\traits\ModifySupportTrait;
@@ -41,5 +43,40 @@ class SettingsService extends Service
     {
         $this->applyDomain($query);
         $this->applyLocale($query);
+    }
+
+    /**
+     * @todo пока рассчитано не небольшое количество настроек, т.к. считывается весь кататлог в статику parent::catalogValue
+     *
+     * Возвращает значение из каталога по ключу
+     * Для использования у search модели должны быть определены методы
+     * getListSearchAttribute и getListSearchKeyAttribute
+     *
+     * @param $key
+     * @return mixed|null
+     * @throws Exception
+     */
+    public function catalogValue($key, $value = null, $type = SettingsTypeEnum::TEXT)
+    {
+        $catalogValue = parent::catalogValue($key);
+        if ($catalogValue){
+            return $catalogValue;
+        }
+
+        if (! $value){
+            return $catalogValue;
+        }
+
+        $formClass = $this->getRelatedFormClass();
+        $form = new $formClass();
+        $form->name = $key;
+        $form->value = $value;
+        $form->type = $type;
+        if (! $this->create($form))
+        {
+            throw new Exception("cannot add setting");
+        }
+
+        return $value;
     }
 }
