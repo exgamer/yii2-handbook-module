230fb9750fe80624b43523cb40c187cd441e55ad
Olzhas Kulzhambekov <exgamer@live.ru>
2020-01-30 14:57:20 +0600

    правка миграции

--- src/views/static-file/_form.php
+++ src/views/static-file/_form.php
@@ -23,69 +23,70 @@ $saveButton = Html::submitButton(
 ?>
 
 <?php Pjax::begin(['formSelector' => '#robots-form']); ?>
-    <?php $form = ActiveForm::begin(['id' => 'static-page-form']); ?>
-        <div class="card">
-            <div class="card-body text-right">
-                <?=  $saveRedirectButton?>
-                <?=  $saveButton?>
-            </div>
+<?php $form = ActiveForm::begin(['id' => 'static-page-form']); ?>
+    <div class="card">
+        <div class="card-body text-right">
+            <?=  $saveRedirectButton?>
+            <?=  $saveButton?>
         </div>
+    </div>
 
-        <div class="card">
-            <div class="card-body">
-                <div class="row">
-                    <div class="col-lg-12 col-md-12 col-sm-12">
-                        <?= $form->field($model, 'filename')->textInput(['maxlength' => true]) ?>
-                    </div>
-                    <div class="col-lg-12 col-md-12 col-sm-12">
-                        <?= $form
-                            ->field($model, 'extension')
-                            ->dropDownList(FileExtensionEnum::labels(), [
-                                'class' => 'form-control form-control-uniform',
-                                'prompt' => Yii::t('yii2admin', 'Выберите тип фаила')
-                            ]);
-                        ?>
-                    </div>
-<!--                    <div class="col-lg-12 col-md-12 col-sm-12">-->
-<!--                        --><?//= $form
-//                            ->field($model, 'type')
-//                            ->dropDownList(StaticFileTypeEnum::labels([StaticFileTypeEnum::SITEMAP, StaticFileTypeEnum::ROBOTS]), [
-//                                'class' => 'form-control form-control-uniform',
-//                                'prompt' => Yii::t('yii2admin', 'Выберите тип фаила')
-//                            ]);
-//                        ?>
-<!--                    </div>-->
-                    <div class="col-lg-12 col-md-12 col-sm-12">
-                        <?= $form->field($model, 'content')->textarea(['style' => 'min-height:350px']) ?>
-                    </div>
-                    <div class="col-lg-6 col-md-6 col-sm-12">
-                        <?= $form
-                            ->field($model, 'status', [
-                                'template' => '
+    <div class="card">
+        <div class="card-body">
+            <div class="row">
+                <div class="col-lg-12 col-md-12 col-sm-12">
+                    <?= $form->field($model, 'filename')->textInput(['maxlength' => true, 'disabled' => ($model->type == StaticFileTypeEnum::ROBOTS ? true : false )]) ?>
+                </div>
+                <div class="col-lg-12 col-md-12 col-sm-12">
+                    <?= $form
+                        ->field($model, 'extension')
+                        ->dropDownList(FileExtensionEnum::labels(), [
+                            'class' => 'form-control form-control-uniform',
+                            'prompt' => Yii::t('yii2admin', 'Выберите тип фаила'),
+                            'disabled' => ($model->type == StaticFileTypeEnum::ROBOTS ? true : false )
+                        ]);
+                    ?>
+                </div>
+                <!--                    <div class="col-lg-12 col-md-12 col-sm-12">-->
+                <!--                        --><?//= $form
+                //                            ->field($model, 'type')
+                //                            ->dropDownList(StaticFileTypeEnum::labels([StaticFileTypeEnum::SITEMAP, StaticFileTypeEnum::ROBOTS]), [
+                //                                'class' => 'form-control form-control-uniform',
+                //                                'prompt' => Yii::t('yii2admin', 'Выберите тип фаила')
+                //                            ]);
+                //                        ?>
+                <!--                    </div>-->
+                <div class="col-lg-12 col-md-12 col-sm-12">
+                    <?= $form->field($model, 'content')->textarea(['style' => 'min-height:350px']) ?>
+                </div>
+                <div class="col-lg-6 col-md-6 col-sm-12">
+                    <?= $form
+                        ->field($model, 'status', [
+                            'template' => '
                                                 <div class="form-check form-check-inline mt-2">
                                                     {input}
                                                 </div>
                                                 {error}
                                             '
-                            ])
-                            ->checkbox(
-                                [
-                                    'label' => Yii::t('yii2admin', 'Активировано'),
-                                    'class' => 'form-check-input-styled-primary',
-                                    'labelOptions' => ['class' => 'form-check-label control-label']
-                                ],
-                                true
-                            )
-                        ?>
-                    </div>
+                        ])
+                        ->checkbox(
+                            [
+                                'label' => Yii::t('yii2admin', 'Активировано'),
+                                'class' => 'form-check-input-styled-primary',
+                                'labelOptions' => ['class' => 'form-check-label control-label']
+                            ],
+                            true
+                        )
+                    ?>
                 </div>
             </div>
         </div>
-        <div class="card">
-            <div class="card-body text-right">
-                <?=  $saveRedirectButton?>
-                <?=  $saveButton?>
-            </div>
+    </div>
+    <div class="card">
+        <div class="card-body text-right">
+            <?=  $saveRedirectButton?>
+            <?=  $saveButton?>
         </div>
-    <?php ActiveForm::end(); ?>
+    </div>
+<?php ActiveForm::end(); ?>
 <?php Pjax::end(); ?>
