60b5512e3513833ecf858ec2df9fe3b599896409
Olzhas Kulzhambekov <exgamer@live.ru>
2020-01-31 13:05:58 +0600

    правка миграции

--- src/services/traits/SitemapGeneratorTrait.php
+++ src/services/traits/SitemapGeneratorTrait.php
@@ -1,6 +1,7 @@
 <?php
 namespace concepture\yii2handbook\services\traits;
 
+use Exception;
 use Yii;
 
 /**
@@ -17,6 +18,48 @@ trait SitemapGeneratorTrait
     public function getSitemapFile()
     {
         return "";
+    }
+
+    /**
+     * Возвращает приоритет элемента для карты саита
+     *
+     * @param $model
+     * @return string
+     * @throws Exception
+     */
+    protected function getPriority($model)
+    {
+        /*
+           Страницы,	изменившиеся	сегодня	–	приоритет	1.0
+           Страницы,	изменившиеся	вчера	–	приоритет	0.9
+           Страницы,	изменившиеся	на	этои 	неделе	–	приоритет	0.8
+           Страницы	новостеи ,	прогнозов	и	др.	(которые	в	RSS)	–	приоритет	не	менее	0.6
+           Все	остальные	страницы	приоритет	-	приоритет	0.5
+         */
+        $last_modified_dt = $model->last_modified_dt;
+        $current_dt = Yii::$app->formatter->asDateTime('now', 'php:Y-m-d H:i:s');
+        $section = $model->section;
+        $dt = new \DateTime($current_dt);
+        if($last_modified_dt > $dt->modify('-20 hours')->format('Y-m-d H:i:s')){
+
+            return '1';
+        }elseif($last_modified_dt > $dt->modify('-40 hours')->format('Y-m-d H:i:s')){
+
+            return '0.9';
+        }elseif($last_modified_dt > $dt->modify('-7 days')->format('Y-m-d H:i:s')){
+
+            return '0.8';
+        }
+
+        /**
+         * @todo тут надо универсальное решение
+         */
+//        elseif(in_array($section, array('news', 'bonus', 'forecast'))){
+//
+//            return '0.6';
+//        }
+
+        return '0.55';
     }
 }
 
