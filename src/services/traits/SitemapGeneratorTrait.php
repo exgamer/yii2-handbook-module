12e3d1dcd0351ae6e8e1fccf2ccfc4db1aaa15d0
Olzhas Kulzhambekov <exgamer@live.ru>
2020-02-02 14:33:27 +0600

    dd

--- src/services/traits/SitemapGeneratorTrait.php
+++ src/services/traits/SitemapGeneratorTrait.php
@@ -6,6 +6,7 @@ use concepture\yii2handbook\enum\StaticFileTypeEnum;
 use concepture\yii2handbook\forms\StaticFileForm;
 use concepture\yii2logic\enum\IsDeletedEnum;
 use concepture\yii2logic\enum\StatusEnum;
+use concepture\yii2logic\helpers\XmlHelper;
 use Exception;
 use Yii;
 use yii\db\ActiveQuery;
@@ -18,39 +19,52 @@ use yii\helpers\ArrayHelper;
 trait SitemapGeneratorTrait
 {
     /**
+     * Новый xml документ
+     * @param string $rootName
+     * @return string
+     */
+    public function getNewDocument($rootName = 'urlset')
+    {
+        $data = [
+            'name' => $rootName, // "name" required, all else optional
+            'attributes' => [
+                'xmlns:xsi' => 'http://www.w3.org/2001/XMLSchema-instance',
+//                'xsi:schemaLocation' => 'http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd',
+                'xmlns' => 'http://www.sitemaps.org/schemas/sitemap/0.9',
+            ],
+
+//            [
+//                'name' => 'url',
+//                [
+//                    'name' => 'loc',
+//                    'value' => 'https://legalbet.ru/best-posts/zhelto-sinij-trend-v-otbore-evro-2016/',
+//                ],
+//                [
+//                    'name' => 'priority',
+//                    'value' => '0.55',
+//                ],
+//            ],
+        ];
+
+        $doc = new \DOMDocument("1.0", "UTF-8");
+        $xslt = $doc->createProcessingInstruction('xml-stylesheet', 'type="text/xsl" href="/sitemap.xsl"');
+        $doc->appendChild($xslt);
+        $child = XmlHelper::generateXmlElement( $doc, $data );
+        if ( $child )
+            $doc->appendChild( $child );
+        $doc->formatOutput = true; // Add whitespace to make easier to read XML
+
+        return $doc->saveXML();
+    }
+
+    /**
      * Возвращает карту саита
      *
      * @return string
      */
     public function getSitemapFile()
     {
-        $data = [
-            'name' => 'urlset', // "name" required, all else optional
-            'attributes' => [
-                'xmlns:xsi' => 'http://www.w3.org/2001/XMLSchema-instance',
-                'xsi:schemaLocation' => 'http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd',
-                'xmlns' => 'http://www.sitemaps.org/schemas/sitemap/0.9',
-            ],
-            [
-                'name' => 'url',
-                [
-                    'name' => 'loc',
-                    'value' => 'https://legalbet.ru/best-posts/zhelto-sinij-trend-v-otbore-evro-2016/',
-                ],
-                [
-                    'name' => 'priority',
-                    'value' => '0.55',
-                ],
-            ],
-        ];
-        $doc = new \DOMDocument("1.0", "UTF-8");
-        $xslt = $doc->createProcessingInstruction('xml-stylesheet', 'type="text/xsl" href="/sitemap.xsl"');
-        $doc->appendChild($xslt);
-        $child = $this->generate_xml_element( $doc, $data );
-        if ( $child )
-            $doc->appendChild( $child );
-        $doc->formatOutput = true; // Add whitespace to make easier to read XML
-        $xml = $doc->saveXML();
+        $xml = $this->getNewDocument();
 
         $form = new StaticFileForm();
         $form->type = StaticFileTypeEnum::SITEMAP;
@@ -60,7 +74,35 @@ trait SitemapGeneratorTrait
         $form->is_hidden = 1;
         Yii::$app->staticFileService->create($form);
 
-        d($xml);
+        $sitemap = Yii::$app->staticFileService->getOneByCondition(['filename' => 'post_part_1']);
+
+
+
+
+        $doc = new \DOMDocument();
+        $doc->loadXML($sitemap->content);
+        $parent = $doc->getElementsByTagName('urlset')->item(0);
+        $url = $doc->createElement("url");
+        $loc = $doc->createElement("loc", "https://legalbet.ru/best-posts/zhelto-sinij-trend-v-otbore-evro-2016/");
+        $priority = $doc->createElement("priority", "0.5");
+        $url->appendChild($loc);
+        $url->appendChild($priority);
+        $parent->appendChild($url);
+
+
+        $xml = $doc->saveXML();
+
+
+        $xml = simplexml_load_string($xml);
+        if ($xml === false) {
+            echo "Failed loading XML: ";
+            foreach(libxml_get_errors() as $error) {
+                echo "<br>", $error->message;
+            }
+        } else {
+            d($xml);
+        }
+
         $sections = $this->getSections();
         dump($sections);
         foreach ($sections as $section){
