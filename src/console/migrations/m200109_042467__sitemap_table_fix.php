1f8b348e7ada1a9f40a629686923aefbe7aaa210
Olzhas Kulzhambekov <exgamer@live.ru>
2020-02-03 12:27:32 +0600

    правка миграции

---
+++ src/console/migrations/m200109_042467__sitemap_table_fix.php
@@ -0,0 +1,36 @@
+<?php
+
+use concepture\yii2logic\console\migrations\Migration;
+
+/**
+ * Class m200109_042467__sitemap_table_fix
+ * @author Olzhas Kulzhambekov <exgamer@live.ru>
+ */
+class m200109_042467__sitemap_table_fix extends Migration
+{
+    function getTableName()
+    {
+        return 'sitemap';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->removeColumn('static_file_id');
+        $this->createColumn("static_filename", $this->string(255));
+        $this->removeColumn('section');
+        $this->createColumn("section", $this->string(255));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        echo "m191121_034605__seo_table_init cannot be reverted.\n";
+
+        return false;
+    }
+}
