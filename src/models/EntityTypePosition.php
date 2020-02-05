ff642fa7663df4225001f04bbcb9b833abc58cb0
Olzhas Kulzhambekov <exgamer@live.ru>
2020-02-05 16:42:17 +0600

    правка миграции

--- src/models/EntityTypePosition.php
+++ src/models/EntityTypePosition.php
@@ -85,7 +85,7 @@ class EntityTypePosition extends ActiveRecord
                 'source' => 'caption'
             ],
             /**
-             * @TODO с этим есть проблема при валидации на мультидоменных проектах
+             * @TODO с этим есть проблема при валидации на мультидоменных проектах из за того что alias и domain_id не required
              */
 //            [
 //                [
