449ce31a697cfb8c54d5b4be0f9100ae1d52792d
Olzhas Kulzhambekov <exgamer@live.ru>
2020-04-02 12:35:50 +0600

    заполнение  и правка таблицы с языками

--- src/console/migrations/m200108_081446_add_default_country.php
+++ src/console/migrations/m200108_081446_add_default_country.php
@@ -14,20 +14,20 @@ class m200108_081446_add_default_country extends Migration
      */
     public function safeUp()
     {
-        $form = new CountryForm();
-        $form->locale = Yii::$app->localeService->catalogKey('ru');
-        $form->iso = "ru";
-        $form->caption = "Россия";
-        $form->status = 1;
-        $form->sort = 0;
-        Yii::$app->countryService->create($form);
-        $form = new CountryForm();
-        $form->locale = Yii::$app->localeService->catalogKey('ru');
-        $form->iso = "kz";
-        $form->caption = "Казахстан";
-        $form->status = 0;
-        $form->sort = 0;
-        Yii::$app->countryService->create($form);
+//        $form = new CountryForm();
+//        $form->locale = Yii::$app->localeService->catalogKey('ru');
+//        $form->iso = "ru";
+//        $form->caption = "Россия";
+//        $form->status = 1;
+//        $form->sort = 0;
+//        Yii::$app->countryService->create($form);
+//        $form = new CountryForm();
+//        $form->locale = Yii::$app->localeService->catalogKey('ru');
+//        $form->iso = "kz";
+//        $form->caption = "Казахстан";
+//        $form->status = 0;
+//        $form->sort = 0;
+//        Yii::$app->countryService->create($form);
     }
 
     /**
