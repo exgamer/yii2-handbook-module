6ea39961f587c21bc5b55398dedf048276b4780b
Olzhas Kulzhambekov <exgamer@live.ru>
2020-04-13 13:47:50 +0600

    домены

--- src/components/routing/DomainUrlManager.php
+++ src/components/routing/DomainUrlManager.php
@@ -9,6 +9,8 @@ use yii\web\UrlManager as YiiUrlManager;
 use yii\web\UrlNormalizer;
 use yii\helpers\Url;
 use concepture\yii2handbook\components\routing\DomainUrlRule;
+use yii\web\UrlRule;
+use yii\web\UrlRuleInterface;
 
 /**
  * @author kamaelkz <kamaelkz@yandex.kz>
@@ -67,6 +69,153 @@ class DomainUrlManager extends YiiUrlManager
     {
         return $this->currentRule;
     }
+
+    /**
+     * ========================================================================
+     * Этот блок копирует методы из родителя
+     * только для того чтобы можно было переопределить метод parent::createUrl
+     * чтобы добавить туда блок для реинита правила
+     */
+
+
+    private $_ruleCache;
+
+    /**
+     * Store rule (e.g. [[UrlRule]]) to internal cache.
+     * @param $cacheKey
+     * @param UrlRuleInterface $rule
+     * @since 2.0.8
+     */
+    protected function setRuleToCache($cacheKey, UrlRuleInterface $rule)
+    {
+        $this->_ruleCache[$cacheKey][] = $rule;
+    }
+
+    /**
+     * Get URL from internal cache if exists.
+     * @param string $cacheKey generated cache key to store data.
+     * @param string $route the route (e.g. `site/index`).
+     * @param array $params rule params.
+     * @return bool|string the created URL
+     * @see createUrl()
+     * @since 2.0.8
+     */
+    protected function getUrlFromCache($cacheKey, $route, $params)
+    {
+        if (!empty($this->_ruleCache[$cacheKey])) {
+            foreach ($this->_ruleCache[$cacheKey] as $rule) {
+                /* @var $rule UrlRule */
+                if (($url = $rule->createUrl($this, $route, $params)) !== false) {
+                    return $url;
+                }
+            }
+        } else {
+            $this->_ruleCache[$cacheKey] = [];
+        }
+
+        return false;
+    }
+
+    /**
+     * Копия родительского с учетом переключения доменов
+     * для корректной генерации урл
+     *
+     * @param $params
+     * @return string
+     * @throws \yii\base\InvalidConfigException
+     */
+    public function createCustomUrl($params)
+    {
+        $params = (array) $params;
+        $anchor = isset($params['#']) ? '#' . $params['#'] : '';
+        unset($params['#'], $params[$this->routeParam]);
+
+        $route = trim($params[0], '/');
+        unset($params[0]);
+
+        $baseUrl = $this->showScriptName || !$this->enablePrettyUrl ? $this->getScriptUrl() : $this->getBaseUrl();
+
+        if ($this->enablePrettyUrl) {
+            $cacheKey = $route . '?';
+            foreach ($params as $key => $value) {
+                if ($value !== null) {
+                    $cacheKey .= $key . '&';
+                }
+            }
+
+            $url = $this->getUrlFromCache($cacheKey, $route, $params);
+            if ($url === false) {
+                /* @var $rule UrlRule */
+                foreach ($this->rules as $rule) {
+                    if (in_array($rule, $this->_ruleCache[$cacheKey], true)) {
+                        // avoid redundant calls of `UrlRule::createUrl()` for rules checked in `getUrlFromCache()`
+                        // @see https://github.com/yiisoft/yii2/issues/14094
+                        continue;
+                    }
+
+                    /**
+                     * От parent::createUrl отличается этим блоком для корректной генерации урл в админке
+                     */
+                    if (isset($rule->patterns) && $rule->patterns && (Yii::$app->domainService->getRealCurrentHost() != Yii::$app->domainService->getCurrentHost())){
+                        $data = Yii::$app->domainService->getCurrentDomainData();
+                        $rule->reinit($data['alias']);
+                    }
+                    /**
+                     *
+                     */
+
+                    $url = $rule->createUrl($this, $route, $params);
+                    if ($this->canBeCached($rule)) {
+                        $this->setRuleToCache($cacheKey, $rule);
+                    }
+                    if ($url !== false) {
+                        break;
+                    }
+                }
+            }
+
+            if ($url !== false) {
+                if (strpos($url, '://') !== false) {
+                    if ($baseUrl !== '' && ($pos = strpos($url, '/', 8)) !== false) {
+                        return substr($url, 0, $pos) . $baseUrl . substr($url, $pos) . $anchor;
+                    }
+
+                    return $url . $baseUrl . $anchor;
+                } elseif (strncmp($url, '//', 2) === 0) {
+                    if ($baseUrl !== '' && ($pos = strpos($url, '/', 2)) !== false) {
+                        return substr($url, 0, $pos) . $baseUrl . substr($url, $pos) . $anchor;
+                    }
+
+                    return $url . $baseUrl . $anchor;
+                }
+
+                $url = ltrim($url, '/');
+                return "$baseUrl/{$url}{$anchor}";
+            }
+
+            if ($this->suffix !== null) {
+                $route .= $this->suffix;
+            }
+            if (!empty($params) && ($query = http_build_query($params)) !== '') {
+                $route .= '?' . $query;
+            }
+
+            $route = ltrim($route, '/');
+            return "$baseUrl/{$route}{$anchor}";
+        }
+
+        $url = "$baseUrl?{$this->routeParam}=" . urlencode($route);
+        if (!empty($params) && ($query = http_build_query($params)) !== '') {
+            $url .= '&' . $query;
+        }
+
+        return $url . $anchor;
+    }
+
+    /**
+     *  Конец
+     * =============================================================================
+     */
 
     /**
      * @inheritDoc
