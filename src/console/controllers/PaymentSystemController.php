a6b7847bf2bc9d66e80310ce584ffa869e4f5776
Eugene Poletaev <eugene@media.bet>
2020-04-29 15:10:26 +0600

    change payment system

---
+++ src/console/controllers/PaymentSystemController.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace concepture\yii2handbook\console\controllers;
+
+use yii\helpers\ArrayHelper;
+use concepture\yii2user\enum\AccessEnum;
+use kamaelkz\yii2admin\v1\actions\SortAction;
+use concepture\yii2logic\actions\web\v2\DeleteAction;
+use kamaelkz\yii2admin\v1\controllers\BaseController;
+use concepture\yii2logic\actions\web\v2\StatusChangeAction;
+use concepture\yii2handbook\actions\PositionSortIndexAction;
+use concepture\yii2handbook\services\EntityTypePositionSortService;
+
+/**
+ * Class PaymentSystemController
+ *
+ * Платежные системы
+ *
+ * @package concepture\yii2handbook\console\controllers
+ * @author Poletaev Eugene <evgstn7@gmail.com>
+ */
+class PaymentSystemController extends BaseController
+{
+    /** @var bool */
+    public $localized = true;
+
+    /**
+     * @inheritDoc
+     */
+    protected function getAccessRules()
+    {
+        $rules = parent::getAccessRules();
+
+        return ArrayHelper::merge(
+            $rules,
+            [
+                [
+                    'actions' => [
+                        'status-change',
+                        PositionSortIndexAction::actionName(),
+                        SortAction::actionName(),
+                    ],
+                    'allow' => true,
+                    'roles' => [
+                        AccessEnum::ADMIN
+                    ],
+                ]
+            ]
+        );
+    }
+
+    /**
+     * @return array
+     */
+    public function actions()
+    {
+        $actions = parent::actions();
+        unset($actions['view']);
+
+        return array_merge($actions,[
+            'status-change' => [
+                'class' => StatusChangeAction::class,
+                'redirect' => false
+            ],
+            'delete' => [
+                'class' => DeleteAction::class,
+                'redirect' => false
+            ],
+            PositionSortIndexAction::actionName() => [
+                'class' => PositionSortIndexAction::class,
+                'entityColumns' => [
+                    'id',
+                    'name',
+                ],
+                'labelColumn' => 'name',
+            ],
+            SortAction::actionName() => [
+                'class' => SortAction::class,
+                'serviceClass' => EntityTypePositionSortService::class
+            ],
+        ]);
+    }
+}
