ff1a79a8538e1dde9b33e06421939f914dfb7597
Eugene Poletaev <eugene@media.bet>
2020-05-14 15:40:55 +0600

    locales captions

--- src/views/payment-system/_form.php
+++ src/views/payment-system/_form.php
@@ -18,10 +18,14 @@ $saveButton = Html::saveButton();
 <?php Pjax::begin(['formSelector' => '#services-form']); ?>
 <?php if (Yii::$app->localeService->catalogCount() > 1): ?>
     <ul class="nav nav-tabs nav-tabs-solid nav-justified bg-light">
+        <?php $locale_id = Yii::$app->localeService->getCurrentLocaleId(); ?>
         <?php foreach (Yii::$app->localeService->getByDomainMap() as $key => $locale):?>
             <li class="nav-item">
                 <?= Html::a(
-                    Yii::$app->localeService->catalogValue($locale, 'locale', 'caption'),
+                    Yii::$app->localeService->catalogValue($locale, 'locale', 'caption', function (\concepture\yii2logic\db\ActiveQuery $query) use ($locale_id) {
+                        $query->resetCondition();
+                        $query->where(['p.locale_id' => $locale_id])->active();
+                    }),
                     \yii\helpers\Url::current(['locale' => $key]),
                     ['class' => 'nav-link ' . ($key ==  $model->locale   ? "active" : "")]
                 ) ?>
