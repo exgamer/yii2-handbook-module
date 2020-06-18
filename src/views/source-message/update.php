a19a111aa5750b5cd7a275b6b3abf6ee67177a4c
Eugeny <evgstn7@gmail.com>
2020-06-18 11:30:12 +0600

    translation modal styles

--- src/views/source-message/update.php
+++ src/views/source-message/update.php
@@ -34,107 +34,115 @@ $saveRedirectButton = Html::saveRedirectButton();
 
 ?>
 
-<div class="container">
-    <?php Pjax::begin(['formSelector' => '#dynamic-elements-form']); ?>
-    <?php $form = ActiveForm::begin(['id' => 'dynamic-elements-form']); ?>
-    <div class="card">
-        <div class="card-body text-right">
-            <?= $saveRedirectButton; ?>
-            <?= $saveButton; ?>
+<?php Pjax::begin(['formSelector' => '#dynamic-elements-form']); ?>
+<?php $form = ActiveForm::begin(['id' => 'dynamic-elements-form']); ?>
+<div class="card">
+    <div class="card-body text-right">
+        <?= $saveRedirectButton; ?>
+        <?= $saveButton; ?>
+    </div>
+</div>
+<div class="row">
+    <div class="col-md-3">
+        <div class="card">
+            <div class="card-header">
+                <legend class="font-weight-semibold text-uppercase font-size-sm">
+                    <?= Yii::t('yii2admin','Язык');?>
+                </legend>
+            </div>
+            <div class="card-body p-0">
+                <ul class="nav nav-tabs nav-tabs-vertical flex-column border-bottom-0">
+                    <?php foreach (array_keys($itemsByLanguage) as $lang): ?>
+                        <li class="nav-item">
+                            <?= Html::a(
+                                $languages[$lang]->caption,
+                                "#tab-{$lang}",
+                                [
+                                    'class' => 'nav-link ' . ('ru' === $lang ? 'active' : null),
+                                    'data-toggle' => 'tab',
+                                ]
+                            ) ?>
+                        </li>
+                    <?php endforeach; ?>
+                </ul>
+            </div>
         </div>
-        <div class="card-body">
-            <div class="row">
-                <div class="col-md-3">
-                    <div class="card">
+    </div>
+
+    <div class="tab-content col-md-9">
+        <?php foreach ($itemsByLanguage as $lang => $items): ?>
+            <div class="tab-pane fade <?= 'ru' === $lang ? 'active' : null;?> show" id="tab-<?= $lang; ?>" role="tabpanel">
+                <div class="card">
+                    <div class="card-header">
                         <legend class="font-weight-semibold text-uppercase font-size-sm">
-                            <?= Yii::t('yii2admin','Язык');?>
+                            <?= Yii::t('yii2admin','Оригинал');?> : <?= $sourceMessage->message ;?>
                         </legend>
-                        <div class="card-body p-0">
-                            <ul class="nav nav-tabs nav-tabs-vertical flex-column border-bottom-0">
-                                <?php foreach (array_keys($itemsByLanguage) as $lang): ?>
-                                    <li class="nav-item">
-                                        <?= Html::a(
-                                            $languages[$lang]->caption,
-                                            "#tab-{$lang}",
-                                            [
-                                                'class' => 'nav-link ' . ('ru' === $lang ? 'active' : null),
-                                                'data-toggle' => 'tab',
-                                            ]
-                                        ) ?>
-                                    </li>
-                                <?php endforeach; ?>
-                            </ul>
+                    </div>
+                    <div class="card-body">
+                        <div class="row">
+                            <?php $i = 0; ?>
+                            <?php foreach ($items as $key => $item) :?>
+                                <div id="<?= $item->language;?>" class="col-lg-12 col-md-12 col-sm-12">
+                                    <?php
+                                    $label = $countries[$item->language]->caption ?? null;
+                                    $language = strtoupper($item->language);
+                                    if($label) {
+                                        $label = "{$label} ({$language})";
+                                    } else {
+                                        $label = $language;
+                                    }
+                                    $label = '<span class="icon flag-' . $countries[$item->language]->iso . ' flag"></span>' . $label;
+                                    ?>
+                                    <?php
+                                    // TODO очень похоже на костыль
+                                    $copyBtn = '
+                                    <span class="source-message-copy btn bg-transparent text-black" style="cursor: pointer;">
+                                        <i class="icon-copy4"></i>
+                                    </span>
+                                ';
+                                    if ($i == 0) {
+                                        $copyBtn = '
+                                        <span class="btn bg-transparent text-white"">
+                                            <i class="icon-copy4"></i>
+                                        </span>
+                                    ';
+                                    }
+                                    ?>
+                                    <?=
+                                    $form
+                                        ->field($model, $item->language, [
+                                            'template' => '
+                                    <div style="margin-left: 46px">{label}</div>
+                                    <div class="input-group">
+                                        <span class="input-group-prepend">'.$copyBtn .'</span>
+                                        {input}
+                                    </div>
+                                    {error}'
+                                        ])
+                                        ->textInput(['value' => $item->translation])
+                                        ->label($label);
+                                    ?>
+                                    <?= $form->field($model, 'ids[]', ['template' => '{input}'])->hiddenInput(['value' => $item->id]);?>
+                                    <?= $form->field($model, 'languages[]', ['template' => '{input}'])->hiddenInput(['value' => $item->language]);?>
+                                </div>
+                                <?php $i++; ?>
+                            <?php endforeach; ?>
                         </div>
                     </div>
                 </div>
-                <div class="tab-content col-md-9">
-                    <?php foreach ($itemsByLanguage as $lang => $items): ?>
-                        <div class="tab-pane fade <?= 'ru' === $lang ? 'active' : null;?> show" id="tab-<?= $lang; ?>" role="tabpanel">
-                            <legend class="font-weight-semibold text-uppercase font-size-sm">
-                                <?= Yii::t('yii2admin','Оригинал');?> : <?= $sourceMessage->message ;?>
-                            </legend>
-                            <div class="row">
-                                <?php $i = 0; ?>
-                                <?php foreach ($items as $key => $item) :?>
-                                    <div id="<?= $item->language;?>" class="col-lg-12 col-md-12 col-sm-12">
-                                        <?php
-                                        $label = $countries[$item->language]->caption ?? null;
-                                        $language = strtoupper($item->language);
-                                        if($label) {
-                                            $label = "{$label} ({$language})";
-                                        } else {
-                                            $label = $language;
-                                        }
-                                        $label = '<span class="icon flag-' . $countries[$item->language]->iso . ' flag"></span>' . $label;
-                                        ?>
-                                        <?php
-                                            // TODO очень похоже на костыль
-                                            $copyBtn = '
-                                                <span class="source-message-copy btn bg-transparent text-black" style="cursor: pointer;">
-                                                    <i class="icon-copy4"></i>
-                                                </span>
-                                            ';
-                                            if ($i == 0) {
-                                                $copyBtn = '
-                                                    <span class="btn bg-transparent text-white"">
-                                                        <i class="icon-copy4"></i>
-                                                    </span>
-                                                ';
-                                            }
-                                        ?>
-                                        <?=
-                                        $form
-                                            ->field($model, $item->language, [
-                                                'template' => '
-                                                <div style="margin-left: 46px">{label}</div>
-                                                <div class="input-group">
-                                                    <span class="input-group-prepend">'.$copyBtn .'</span>
-                                                    {input}
-                                                </div>
-                                                {error}'
-                                            ])
-                                            ->textInput(['value' => $item->translation])
-                                            ->label($label);
-                                        ?>
-                                        <?= $form->field($model, 'ids[]', ['template' => '{input}'])->hiddenInput(['value' => $item->id]);?>
-                                        <?= $form->field($model, 'languages[]', ['template' => '{input}'])->hiddenInput(['value' => $item->language]);?>
-                                    </div>
-                                    <?php $i++; ?>
-                                <?php endforeach; ?>
-                            </div>
-                        </div>
-                    <?php endforeach; ?>
-                </div>
+
             </div>
-        </div>
-        <div class="card-body text-right">
-            <?= $saveRedirectButton; ?>
-            <?= $saveButton; ?>
-        </div>
+        <?php endforeach; ?>
     </div>
-    <?php ActiveForm::end(); ?>
-    <?php Pjax::end(); ?>
 </div>
+<div class="card">
+    <div class="card-body text-right">
+        <?= $saveRedirectButton; ?>
+        <?= $saveButton; ?>
+    </div>
+</div>
+<?php ActiveForm::end(); ?>
+<?php Pjax::end(); ?>
 
 
 
