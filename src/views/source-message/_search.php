26258b705aa6170783d62ea8ccb401e9f5b929b4
alexandrtspl <alexandr@media.bet>
2020-06-18 16:21:17 +0600

    add category filter to sourcemessage admin

--- src/views/source-message/_search.php
+++ src/views/source-message/_search.php
@@ -1,8 +1,13 @@
 <div class="row">
-    <div class="col-lg-6 col-md-6 col-sm-12">
+    <div class="col-lg-4 col-md-6 col-sm-12">
         <?= $form->field($model,'message')->textInput();?>
     </div>
-    <div class="col-lg-6 col-md-6 col-sm-12">
+    <div class="col-lg-4 col-md-6 col-sm-12">
         <?= $form->field($model,'translation')->textInput();?>
     </div>
+    <?php if (!\Yii::$app->user->can(AccessEnum::SUPERADMIN)) { ?>
+    <div class="col-lg-4 col-md-6 col-sm-12">
+        <?= $form->field($model,'category')->textInput();?>
+    </div>
+    <?php } ?>
 </div>
