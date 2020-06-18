a8ec573cc26dbca0040c63fd5d13288eeb6d61d8
alexandrtspl <alexandr@media.bet>
2020-06-18 16:39:53 +0600

    fix use

--- src/views/source-message/_search.php
+++ src/views/source-message/_search.php
@@ -1,3 +1,6 @@
+<?php
+    use concepture\yii2logic\enum\AccessEnum;
+?>
 <div class="row">
     <div class="col-lg-4 col-md-6 col-sm-12">
         <?= $form->field($model,'message')->textInput();?>
