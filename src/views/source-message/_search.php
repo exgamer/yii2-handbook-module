df18cced2f7af5b92877283b0221183611c26978
alexandrtspl <alexandr@media.bet>
2020-06-18 16:45:58 +0600

    up

--- src/views/source-message/_search.php
+++ src/views/source-message/_search.php
@@ -1,6 +1,3 @@
-<?php
-    use concepture\yii2logic\enum\AccessEnum;
-?>
 <div class="row">
     <div class="col-lg-4 col-md-6 col-sm-12">
         <?= $form->field($model,'message')->textInput();?>
@@ -8,9 +5,4 @@
     <div class="col-lg-4 col-md-6 col-sm-12">
         <?= $form->field($model,'translation')->textInput();?>
     </div>
-    <?php if (!\Yii::$app->user->can(AccessEnum::SUPERADMIN)) { ?>
-    <div class="col-lg-4 col-md-6 col-sm-12">
-        <?= $form->field($model,'category')->textInput();?>
-    </div>
-    <?php } ?>
-</div>
+</div> 
