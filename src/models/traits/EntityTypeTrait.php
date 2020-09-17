53e74817ef2217cc61a2f5b0e9b966329a1a3333
Eugeny <evgstn7@gmail.com>
2020-09-17 16:49:48 +0600

    EntityTypeTrait: add getEntityCaption method

--- src/models/traits/EntityTypeTrait.php
+++ src/models/traits/EntityTypeTrait.php
@@ -9,17 +9,33 @@ use concepture\yii2handbook\models\EntityType;
  */
 trait EntityTypeTrait
 {
+    /**
+     * @return mixed
+     */
     public function getEntityType()
     {
         return $this->hasOne(EntityType::class, ['id' => 'entity_type_id']);
     }
 
+    /**
+     * @return string|null
+     */
     public function getEntityName()
     {
         if (isset($this->entityType)){
             return $this->entityType->table_name;
         }
 
+        return null;
+    }
+    /**
+     * @return string|null
+     */
+    public function getEntityCaption()
+    {
+        if (isset($this->entityType)){
+            return $this->entityType->caption;
+        }
         return null;
     }
 }
