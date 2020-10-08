70bfd973fd5e4ca4fafddfad8bc39447bc211cff
i.skiba <ilya@mediabet.kz>
2020-10-08 11:07:48 +0000

    fix: plural

--- src/models/behaviors/PluralMessageBehavior.php
+++ src/models/behaviors/PluralMessageBehavior.php
@@ -81,16 +81,16 @@ class PluralMessageBehavior extends Behavior
                     $replaceString .= "{$type}{{$value}} ";
                 }
 
-                if (strpos($this->owner->{$target}, $this->token) === false) {
-                    $this->owner->addError($target, Yii::t('common', 'Необходимо указать токен {:token}', [
-                        ':token' => $this->token,
-                    ]));
-
-                    return;
+                if (strpos($this->owner->{$target}, $this->token) !== false) {
+                    $replaceString = trim($replaceString);
+                    $this->owner->{$target} = str_replace($this->token, "{n, plural, {$replaceString}}", $this->owner->{$target});
+                } else {
+                    if (! empty($this->owner->{$target})) {
+                        $this->owner->addError($target, Yii::t('common', 'Необходимо указать токен {:token}', [
+                            ':token' => $this->token,
+                        ]));
+                    }
                 }
-
-                $replaceString = trim($replaceString);
-                $this->owner->{$target} = str_replace($this->token, "{n, plural, {$replaceString}}", $this->owner->{$target});
             }
         }
     }
