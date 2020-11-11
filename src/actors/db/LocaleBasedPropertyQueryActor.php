4c462c2a283c6ca74151c07f52da227d09692b1f
Olzhas Kulzhambekov <exgamer@live.ru>
2020-11-11 15:52:39 +0600

    ++

---
+++ src/actors/db/LocaleBasedPropertyQueryActor.php
@@ -0,0 +1,21 @@
+<?php
+namespace concepture\yii2handbook\actors\db;
+
+use concepture\yii2logic\actors\db\QueryActor;
+use Yii;
+use concepture\yii2logic\db\HasPropertyActiveQuery;
+
+/**
+ * Class LocaleBasedPropertyQueryActor
+ * @package concepture\yii2logic\actors\db
+ * @author Olzhas Kulzhambekov <exgamer@live.ru>
+ */
+class LocaleBasedPropertyQueryActor extends QueryActor
+{
+    public function run()
+    {
+        if ($this->query instanceof HasPropertyActiveQuery) {
+            $this->query->applyPropertyUniqueValue(['domain_id' => Yii::$app->domainService->getCurrentDomainId(), 'locale_id' => 1]);
+        }
+    }
+}
